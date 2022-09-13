bdd4e6a964c7253692fd2024e9fbb38239d5e2ae
kim <rlarkdgh1056@naver.com>
2022-09-13 17:27:49 +0900

    add V5,Adapter

---
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV3HandlerAdapter.java
@@ -0,0 +1,35 @@
+package hello.servlet.web.frontcontroller.v5.adapter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+
+    //ControllerV3의 Interface로 구현한 무언가가 넘어오면 true, 아니면 false 반환
+   @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controllerV3 = (ControllerV3)handler;
+        HashMap<String, String> paramMap = createParamMap(request);
+        ModelView mv = controllerV3.process(paramMap);
+       return mv;
+    }
+
+    private HashMap<String, String> createParamMap(HttpServletRequest request) {
+        HashMap<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
